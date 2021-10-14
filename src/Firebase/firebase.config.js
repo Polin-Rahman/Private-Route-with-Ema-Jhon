6e65f0a5d6dd199ae999cc1ad97c8199ea539720
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-10-14 17:23:49 +0600

    private route & firebase auth

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBKG_AvqXXyNP54gyfiiQbM2no3aD_9NA",
+    authDomain: "ema-jhon-simple-d0680.firebaseapp.com",
+    projectId: "ema-jhon-simple-d0680",
+    storageBucket: "ema-jhon-simple-d0680.appspot.com",
+    messagingSenderId: "28035879916",
+    appId: "1:28035879916:web:a58b75f92bd720dccce6c9"
+};
+
+export default firebaseConfig;
